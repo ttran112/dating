16bbd95e7207bacbf4e674ba25c2f26aea5e0655
ttran112 <ttran112@mail.greenriver.edu>
2021-03-04 17:32:13 -0500

    change datalayer and validation to class

--- model/validation.php
+++ model/validation.php
@@ -1,64 +1,62 @@
 <?php
-
-
-function validName($name)
+class validation
 {
-    return !empty($name) && ctype_alpha($name);
-}
-
-function validAge($age)
-{
-    return !empty($age) && is_numeric($age);
-}
-
-function validGenders($gender)
-{
-    $validGenders = getGenders();
-    return in_array($gender, $validGenders);
-//    $validGenders = getGenders();
-//    foreach ($selectedGender as $selected) {
-//        if(!in_array($selected,$validGenders)){
-//            return false;
-//        }
-//    }
-//    return true;
-
-}
-
-function validPhone($phone)
-{
-    return !empty($phone) && preg_match("/^\d{10}$/",$phone);
-}
-
-function validEmail($email)
-{
-    return !empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL);
-}
-
-//function validBio($bio){
-//    return preg_match("/.{2,60}$/", $bio);
-//}
-
-function validIndoor($selectedIndoors)
-{
-    $validIndoors = getIndoorInterest();
-    foreach ($selectedIndoors as $selected) {
-        if (!in_array($selected,$validIndoors)){
-            return false;
-        }
+    private $_dataLayer;
+    function __construct()
+    {
+        $this->_dataLayer = new DataLayer();
     }
-    return true;
-}
 
-function validOutDoor($selectedOutdoors)
-{
-    $validOutdoors = getOutdoorInterest();
-    foreach ($selectedOutdoors as $selected) {
-        if (!in_array($selected,$validOutdoors)){
-            return false;
-        }
+    function validName($name)
+    {
+        return !empty($name) && ctype_alpha($name);
     }
-    return true;
+
+    function validAge($age)
+    {
+        return !empty($age) && is_numeric($age);
+    }
+
+    function validGenders($gender)
+    {
+        $validGenders = $this->_dataLayer->getGenders();
+        return in_array($gender, $validGenders);
+
+
+    }
+
+    function validPhone($phone)
+    {
+        return !empty($phone) && preg_match("/^\d{10}$/", $phone);
+    }
+
+    function validEmail($email)
+    {
+        return !empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+
+    function validIndoor($selectedIndoors)
+    {
+        $validIndoors = $this->_dataLayer->getIndoorInterest();
+        foreach ($selectedIndoors as $selected) {
+            if (!in_array($selected, $validIndoors)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function validOutDoor($selectedOutdoors)
+    {
+        $validOutdoors = $this->_dataLayer->getOutdoorInterest();
+        foreach ($selectedOutdoors as $selected) {
+            if (!in_array($selected, $validOutdoors)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 
